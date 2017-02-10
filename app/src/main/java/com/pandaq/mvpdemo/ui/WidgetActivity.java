425d4d4077e7a0f375c1e3286c2723cbb413c492
PandaQ <767807368@qq.com>
2017-02-10 15:31:18 +0800

    更新修复 bug 后的 LoopScaleView

--- app/src/main/java/com/pandaq/mvpdemo/ui/WidgetActivity.java
+++ app/src/main/java/com/pandaq/mvpdemo/ui/WidgetActivity.java
@@ -2,9 +2,13 @@ package com.pandaq.mvpdemo.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.pandaq.loopscaleview.LoopScaleView;
 import com.pandaq.mvpdemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by PandaQ on 2017/1/13.
@@ -12,9 +16,21 @@ import com.pandaq.mvpdemo.R;
  */
 
 public class WidgetActivity extends BaseActivity {
+    @BindView(R.id.lsv_scale)
+    LoopScaleView mLsvScale;
+    @BindView(R.id.tv_value)
+    TextView mTvValue;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_widget);
+        ButterKnife.bind(this);
+        mLsvScale.setOnValueChangeListener(new LoopScaleView.OnValueChangeListener() {
+            @Override
+            public void OnValueChange(int newValue) {
+                mTvValue.setText(String.valueOf(newValue));
+            }
+        });
     }
 }
