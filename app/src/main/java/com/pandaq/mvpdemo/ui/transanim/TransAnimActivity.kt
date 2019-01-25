6554ba0510cbb04aa0d148c9e4f33301c542efb2
PandaQ <panda.h@foxmail.com>
2019-01-25 18:44:48 +0800

    [dev] 转场动画

--- app/src/main/java/com/pandaq/mvpdemo/ui/transanim/TransAnimActivity.kt
+++ app/src/main/java/com/pandaq/mvpdemo/ui/transanim/TransAnimActivity.kt
@@ -1,9 +1,14 @@
 package com.pandaq.mvpdemo.ui.transanim
 
+import android.animation.FloatEvaluator
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.os.Bundle
-import android.support.v4.app.ActivityOptionsCompat
+import android.support.v4.app.ActivityOptionsCompat.makeSceneTransitionAnimation
+import android.support.v4.util.Pair
 import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
 import com.pandaq.mvpdemo.R
 import com.pandaq.mvpdemo.ui.BaseActivity
 import kotlinx.android.synthetic.main.activity_trans_anim.*
@@ -16,11 +21,21 @@ import kotlinx.android.synthetic.main.activity_trans_anim.*
  */
 class TransAnimActivity : BaseActivity() {
 
-    var distance = 0f
-    var downY = 0f
-    var screenWidth = 900f
-    var transFactor = 0f
-    var envFactor = 0f
+    private val STATE_COLLAPSE = 0
+    private val STATE_EXPAND = 1
+    private var viewPause = false
+
+    private var distance = 0f
+    private var downY = 0f
+    // 覆盖View Y 轴初始偏移量
+    private var initCoverTransY = 0f
+    private var screenWidth = 900f
+    private var transFactor = 0f
+    private var envFactor = 0f
+    private lateinit var evaluator: FloatEvaluator
+    private lateinit var collapseAnim: ValueAnimator
+    private lateinit var expandAnim: ValueAnimator
+    private var cardState = STATE_COLLAPSE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,20 +44,56 @@ class TransAnimActivity : BaseActivity() {
     }
 
     private fun initView() {
+        evaluator = FloatEvaluator()
+        collapseAnim = ValueAnimator.ofFloat(0.0f, 1f)
+        collapseAnim.duration = 200
+        expandAnim = ValueAnimator.ofFloat(0.0f, 1f)
+        expandAnim.duration = 200
+
         card_cover.setOnTouchListener { v, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
+                    viewPause = false
+                    collapseAnim.cancel()
+                    expandAnim.cancel()
                     downY = event.rawY
-                    distance = 0f
+                    initCoverTransY = card_cover.translationY
                 }
                 MotionEvent.ACTION_MOVE -> {
-                    distance = event.rawY - downY // 上滑距离
-                    scrollCover(distance)
+                    distance = event.rawY - downY + initCoverTransY // 上滑距离
+                    if (-distance > card_cover.measuredHeight * 0.18f) {
+                        distance = -card_cover.measuredHeight * 0.18f
+                    }
+                    if (distance > 0) {
+                        distance = 0f
+                    }
+                    if (cardState == STATE_EXPAND && distance <= -card_cover.measuredHeight * 0.18f) { // 展开状态拉到顶继续上拉
+                        if (!viewPause) {
+                            startDetail()
+                        }
+                    } else {
+                        scrollCover(distance)
+                    }
                 }
                 MotionEvent.ACTION_UP -> {
-                    if (-distance >= card_cover.measuredHeight * 0.4f) { // 未滑动到触发位置，恢复界面
-                        startDetail()
-                        distance = 0f
+                    if (cardState == STATE_COLLAPSE) { //当前还未到达展开状态
+                        if (-distance >= card_cover.measuredHeight * 0.18f / 2) {
+                            cardState = STATE_EXPAND
+                            // 动画回到展开状态
+                            expand()
+                        } else {
+                            // 动画回到折叠状态
+                            collapse()
+                        }
+                    } else { //当前还未到达关闭状态
+                        if (-distance < card_cover.measuredHeight * 0.18f / 2) { // 下滑超过一半折叠
+                            cardState = STATE_COLLAPSE
+                            // 动画回到折叠状态
+                            collapse()
+                        } else {
+                            // 动画回到展开状态
+                            expand()
+                        }
                     }
                 }
             }
@@ -51,28 +102,46 @@ class TransAnimActivity : BaseActivity() {
     }
 
     private fun startDetail() {
+        viewPause = true
         val intent = Intent(this, DetailInfoActivity::class.java)
-        val options = ActivityOptionsCompat
-                .makeSceneTransitionAnimation(this,
-                        card_cover, "imageHeader")
+        val options = makeSceneTransitionAnimation(this,
+                Pair<View, String>(findViewById(R.id.card_cover), "imageHeader"),
+                Pair<View, String>(findViewById(R.id.card_content), "textContent"))
         startActivity(intent, options.toBundle())
     }
 
-    private fun scrollCover(float: Float) {
-        transFactor = (screenWidth / card_content.measuredWidth - 1) / (card_cover.measuredHeight * 0.4f)
-        envFactor = 32 / (card_cover.measuredHeight * 0.4f)
-        if (distance <= 0 && -distance < card_cover.measuredHeight * 0.4f) { // 只允许上滑卡片
-            card_cover.translationY = float
-            card_cover.elevation = -envFactor * float + 16
-            card_content.translationY = -float
-            card_content.scaleX = 1 - transFactor * float
-            card_content.scaleY = 1 - transFactor * float
+    private fun scrollCover(distance: Float) {
+        transFactor = 1.0f / card_cover.measuredHeight
+        envFactor = 32 / (card_cover.measuredHeight * 0.18f)
+        if (distance <= 0 && -distance < card_cover.measuredHeight * 0.18f) { // 只允许上滑卡片
+            card_cover.translationY = distance
+            card_content.translationY = -distance
+            card_content.scaleX = 1 + 0.4f * (-distance / (card_cover.measuredHeight * 0.18f)) //最大扩大到 1.2 倍
+            card_content.scaleY = 1 + 0.4f * (-distance / (card_cover.measuredHeight * 0.18f))
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        scrollCover(0f)
+    private fun expand() {
+        val currentTransY = card_cover.translationY
+        expandAnim.addUpdateListener { animation ->
+            val float = animation.animatedValue as Float
+            distance = evaluator.evaluate(float, currentTransY,
+                    -card_cover.measuredHeight * 0.18f)
+            scrollCover(distance)
+        }
+        scrollCover(-card_cover.measuredHeight * 0.18f)
+        expandAnim.start()
+    }
+
+    private fun collapse() {
+        val currentTransY = card_cover.translationY
+        collapseAnim.addUpdateListener { animation ->
+            val float = animation.animatedValue as Float
+            val distance = evaluator.evaluate(float, currentTransY,
+                    0)
+            scrollCover(distance)
+        }
+        collapseAnim.start()
     }
 
 }
